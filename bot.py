627d1aa9a8bb2e70d221ad5a581cf59ca55a3ba4
Long Nguyen <long.vnguyenn@gmail.com>
2017-05-15 18:50:51 -0400

    Request info from Uber

--- bot.py
+++ bot.py
@@ -17,13 +17,18 @@ client = UberRidesClient(session)
 
 class User:
     def __init__(self):
-        self.ip          = ""
-        self.current_lat = -1
-        self.current_lng = -1
-        self.query       = ""
-        self.dest_lat    = -1
-        self.dest_lng    = -1
-        self.dest_addr   = ""
+        self.ip             = ""
+        self.current_lat    = -1
+        self.current_lng    = -1
+        self.query          = ""
+        self.dest_lat       = -1
+        self.dest_lng       = -1
+        self.dest_addr      = ""
+        self.num_seat       = -1
+        self.U_price_range  = ""
+        self.L_price_range  = ""
+        self.uber_ETA       = -1
+        self.lyft_ETA       = -1
 
 
 
@@ -40,6 +45,24 @@ class User:
         self.dest_lat = get_info["results"][0]["geometry"]["location"]["lat"]
         self.dest_lng = get_info["results"][0]["geometry"]["location"]["lng"]
         self.dest_addr = get_info["results"][0]["formatted_address"]
+
+    def get_uber_info(self):
+        # Get price info from Uber
+        response_price = client.get_price_estimates(start_latitude=self.current_lat, start_longitude=self.current_lng,
+                                            end_latitude=self.dest_lat, end_longitude=self.dest_lng)
+        estimate_price = response_price.json.get("prices")
+
+        # Get ETA info from Uber
+        headers = {"Authorization": "Token {}".format(uber_svtoken)}
+        params = {"start_latitude": self.current_lat, "start_longitude": self.current_lng}
+        url = "https://api.uber.com/v1.2/estimates/time?"
+        estimate_time = requests.get(url, headers=headers, params=params).json()["times"]
+
+        num_seat = self.num_seat
+        if (num_seat <= 2):
+            self.uber_ETA = estimate_time[0]["estimate"] / 60
+            self.U_price_range = estimate_price[0]["estimate"]
+
 
 
 
@@ -69,8 +92,8 @@ def process_current_location(message):
         lng = message.location.longitude
         user = User()
         user_dict[message.chat.id] = user
-        user.lat = lat
-        user.lng = lng
+        user.current_lat = lat
+        user.current_lng = lng
         msg = bot.send_message(message.chat.id, "Cool! So where are you heading?")
         bot.register_next_step_handler(msg, process_destination)
 
@@ -113,6 +136,16 @@ def number_people(message):
     else:
         msg = bot.send_message(message.chat.id, "Hmmm.. Can you retype your destination?")
         bot.register_next_step_handler(msg, process_destination)
+
+
+def process_order(message):
+    response = message.text
+    user = user_dict[message.chat.id]
+    user.num_seat = int(response)
+    user.get_uber_info()
+    msg = bot.send_message(message.chat.id, "Uber:  ETA {} minutes - Price range {}".format(user.uber_ETA, user.U_price_range))
+
+
 
 
 bot.polling()
